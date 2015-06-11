c86cd6c10b8969c533649e74e54addddae44719e
Jhonryl Maniquez <jmaniquez@vjsol.jp>
2015-06-11 16:21:24 +0800

    start data

---
+++ html/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>オンライン英会話ならskype（スカイプ）不要のネイティブキャンプ</title>
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta http-equiv="Content-Script-Type" content="text/javascript">
+<meta http-equiv="Content-Style-Type" content="text/css">
+<link rel="stylesheet" type="text/css" href="./css/html5reset.css">
+<link rel="stylesheet" type="text/css" href="./css/common.css">
+<link rel="stylesheet" type="text/css" href="./css/webtext.css">
+<!--[if lt IE 9]>
+<script src="js/html5shiv.js"></script>
+<![endif]-->
+</head>
+<body style="background:#fff;min-width:inherit;">
+<div class="webtext" id="webtext">
+<?php include './textbook/' . $_GET['level'] . '/' . $_GET['chapter'] . '.html'; ?>
+</div>
+</body>
+</html>
